ed392c8c8c0605083fd717821b3598134df7dce0
Nick Schuch <nick@myschuch.com>
2016-03-22 12:27:16 +1000

    Forward host

--- src/github.com/previousnext/kube-ingress/nginx.go
+++ src/github.com/previousnext/kube-ingress/nginx.go
@@ -31,6 +31,10 @@ http {
     server {
         listen      {{ $.Port }};
         server_name {{ $sd }};
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
 
 {{ range $ld, $location := $servers }}
         location {{ $location.Path }} {
